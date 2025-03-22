2463f0767c1973d39f302e1f78c494d9e4107fca
Kirill Novichkov <sufirhavat@gmail.com>
2025-03-22 12:39:46 +0700

    added changable button

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,18 +3,56 @@ package ru.netology.nmedia
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Автор поста",
+            content = "Соедржимое поста",
+            published = "22.03.2025",
+            likeByMe = false
+        )
+
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+
+            avatar.setImageResource(R.drawable.post_avatar_drawable)
+
+            var shareCountLong: Long = 0
+
+            like.setImageResource(R.drawable.ic_like)
+            likeCount.text = "0"
+            like.setOnClickListener {
+                post.likeByMe = !post.likeByMe
+
+                if(post.likeByMe) likeCount.text = "1"
+                else likeCount.text = "0"
+
+                like.setImageResource(
+                    if (post.likeByMe) R.drawable.ic_liked_24
+                    else R.drawable.ic_like
+                )
+            }
+
+            share.setImageResource(R.drawable.ic_share)
+            shareCount.text = shareCountLong.toString()
+            share.setOnClickListener {
+                shareCountLong ++
+                shareCount.text = shareCountLong.toString()
+
+            }
+
+            views.setImageResource(R.drawable.ic_views)
+            viewsCount.text = "0"
         }
     }
 }
