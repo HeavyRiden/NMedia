db329692c14abfd32655bd05e23dcb6e6cb475fa
Kirill Novichkov <sufirhavat@gmail.com>
2025-03-26 08:17:01 +0700

    some experiments

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -20,22 +20,20 @@ class MainActivity : AppCompatActivity() {
                 author.text = post.author
                 published.text = post.published
                 content.text = post.content
-
-                avatar.setImageResource(R.drawable.post_avatar_drawable)
-
-                like.setImageResource(
-                    if (post.likeByMe) R.drawable.ic_liked_24
-                    else R.drawable.ic_like
-                )
-                likeCount.text = numLogic(post.countLikes)
-
-
-                share.setImageResource(R.drawable.ic_share)
-                shareCount.text = numLogic(post.countShare)
-
-                views.setImageResource(R.drawable.ic_views)
-                viewsCount.text = numLogic(post.countViews)
+                
             }
+            binding.like.setImageResource(
+                if (post.likeByMe) R.drawable.ic_liked_24
+                else R.drawable.ic_like
+            )
+            binding.likeCount.text = numLogic(post.countLikes)
+            binding.avatar.setImageResource(R.drawable.post_avatar_drawable)
+
+            binding.share.setImageResource(R.drawable.ic_share)
+            binding.shareCount.text = numLogic(post.countShare)
+
+            binding.views.setImageResource(R.drawable.ic_views)
+            binding.viewsCount.text = numLogic(post.countViews)
 
             binding.like.setOnClickListener() {
                 viewModel.like()
